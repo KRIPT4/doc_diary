7de6f64e0c535d3a2ce775ba02809a82f55724c1
yangfengpu <fengpuy@gmail.com>
2015-03-20 11:18:09 +0800

    API environment setting

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,10 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
-
+extensions = ['sphinxcontrib.httpdomain']
+http_index_ignore_prefixes = ['/internal', '/_proxy']
+http_index_shortname = 'api'
+http_index_shortname = "My Project HTTP API"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
